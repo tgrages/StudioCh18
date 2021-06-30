435097da3bb30dc389db1dc77e7606830d2e455f
NestoMedina <emedina670@hotmail.com>
2021-06-30 11:11:15 -0400

    working day 3

--- ConsoleApp1/ConsoleApp1/MenuItems.cs
+++ ConsoleApp1/ConsoleApp1/MenuItems.cs
@@ -11,53 +11,44 @@ namespace ConsoleApp1
         public string _itemName;
         private double _price;
         private int _calories;
-        public DateTime InputDate = new DateTime();
+        public DateTime InputDate { get; set; }
+        public int ItemAge;
 
-
-        /*
-        public void ItemName(string name)
+        public int age
         {
-            _itemName.Add(name);
-        } 
-
-        public double Price
-        {
-            get { return _price; }
-            set { _price = value; }
+            get
+            {
+                TimeSpan timeSpan = DateTime.Today - InputDate;
+                int days = timeSpan.Days;
+                return this.ItemAge = days;
+            }
         }
-
-        public string Calories
-        {
-            get { return _calories; }
-            set { _calories = value; }
-        } */
 
         public void PrintDetails()
         {
             Console.WriteLine($"{this._itemName} is ${this._price} and has {this._calories} calories.");
-            DateTime thisDay = DateTime.Today;
 
-            //new variable representing how many days before and do math
-            //create int variable (A) that determines days
-            //conditional statement that compares (A) to thisDay.
-
-            TimeSpan timeSpan = thisDay - new DateTime(1995, 12, 20);
-            Console.WriteLine(timeSpan);
-            //if (this.DateTime <= )
-            //{
-            //    Console.WriteLine($"{this._itemName} is a new item!");
-            //}
+           if (this.ItemAge.Equals(this.InputDate))
+           {
+                Console.WriteLine($"{this._itemName} is a new item!");
+           }
+           else
+            {
+                Console.WriteLine($"{this._itemName} is an old item.");
+            }
 
         }
 
         
-        public MenuItems(string name, double price, int cal, DateTime date)
+        public MenuItems(string name, double price, int cal, DateTime time)
         {
             this._itemName = name;
             this._price = price;
             this._calories = cal;
-            this.InputDate = date;
+            this.InputDate = time;
         }
+
+
 
 
 
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,43 +8,46 @@ namespace ConsoleApp1
         {
             Menu TimeUpdate = new Menu();
             TimeUpdate.LastUpdated = "June 29, 2021";
-            //Console.WriteLine("Menu Last Updated on " + TimeUpdate.LastUpdated);
+            Console.WriteLine("Menu Last Updated on " + TimeUpdate.LastUpdated);
+            //may need to make this a method and field in one of classes
+            DateTime inputDate = new DateTime(2021, 06, 20);
 
             //Appetizers
             Menu AppTitle = new Menu("Appetizer");
-            MenuItems AppItem1 = new MenuItems("Buffalo Wings", 5.99, 500, DateTime.Today);
-            MenuItems AppItem2 = new MenuItems("Blooming Onion", 7.99, 800, DateTime.Today);
+            MenuItems AppItem1 = new MenuItems("Buffalo Wings", 5.99, 500, inputDate);
+            MenuItems AppItem2 = new MenuItems("Blooming Onion", 7.99, 800, inputDate);
             AppTitle.AddMenuItem(AppItem1);
             AppTitle.AddMenuItem(AppItem2);
 
+   
             Console.WriteLine("\n" + AppTitle.MenuName);
             AppTitle.PrintMenuItems();
 
 
             //Entrees
             Menu EntreesTitle = new Menu("Entrees");
-            MenuItems EntreesItem1 = new MenuItems("Hamburgers", 12.99, 900, DateTime.Today);
-            MenuItems EntreesItem2 = new MenuItems("Tacos", 9.99, 650, DateTime.Today);
-            MenuItems EntreesItem3 = new MenuItems("Spaghetti", 15.99, 1000, DateTime.Today);
+            MenuItems EntreesItem1 = new MenuItems("Hamburgers", 12.99, 900, inputDate);
+            MenuItems EntreesItem2 = new MenuItems("Tacos", 9.99, 650, inputDate);
+            MenuItems EntreesItem3 = new MenuItems("Spaghetti", 15.99, 1000, inputDate);
             EntreesTitle.AddMenuItem(EntreesItem1);
             EntreesTitle.AddMenuItem(EntreesItem2);
             EntreesTitle.AddMenuItem(EntreesItem3);
 
-            //Console.WriteLine("\n" + EntreesTitle.MenuName);
-            //EntreesTitle.PrintMenuItems();
+           Console.WriteLine("\n" + EntreesTitle.MenuName);
+           EntreesTitle.PrintMenuItems();
 
 
             //Dessert
             Menu DessertTitle = new Menu("Dessert");
-            MenuItems DessertItem1 = new MenuItems("Key Lime Pie", 7.99, 600, DateTime.Today);
-            MenuItems DessertItem2 = new MenuItems("Ice Cream Sundae", 9.99, 700, DateTime.Today);
-            MenuItems DessertItem3 = new MenuItems("Red Velvet Cake", 8.99, 500, DateTime.Today);
+            MenuItems DessertItem1 = new MenuItems("Key Lime Pie", 7.99, 600, inputDate);
+            MenuItems DessertItem2 = new MenuItems("Ice Cream Sundae", 9.99, 700, inputDate);
+            MenuItems DessertItem3 = new MenuItems("Red Velvet Cake", 8.99, 500, inputDate);
             DessertTitle.AddMenuItem(DessertItem1);
             DessertTitle.AddMenuItem(DessertItem2);
             DessertTitle.AddMenuItem(DessertItem3);
 
-            //Console.WriteLine("\n" + DessertTitle.MenuName);
-            //DessertTitle.PrintMenuItems();
+           Console.WriteLine("\n" + DessertTitle.MenuName);
+           DessertTitle.PrintMenuItems();
         }
     }
 }
